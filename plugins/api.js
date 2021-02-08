5f3560f4b56f7a1bdd59b1bf9b58e67094ff13b8
comfuture <comfuture@iplateia.com>
2021-02-09 01:24:21 +0900

    fixes $api plugin implement

--- plugins/api.js
+++ plugins/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export default (ctx, inject) => {
-  const api = function(url, ...options) {
+  const api = function(url, options = {}, method = 'get', data = null) {
     options['headers'] = Object.assign({}, options['headers'], {'Accept': 'application/json'})
 
     // XXX Only axios GET request alleviates some discomfort of using options.params instead of options.query among options.
@@ -21,9 +21,9 @@ export default (ctx, inject) => {
         baseURL: `http://localhost:${port}`,
         headers
       })
-      return http(url, ...options).then(r => r.data)
+      return http(url, options, method, data).then(r => r.data)
     }
-    return axios(url, ...options).then(r => r.data)
+    return axios(url, options, method, data).then(r => r.data)
   }
 
   /**
@@ -38,25 +38,25 @@ export default (ctx, inject) => {
 
 
   /** @public @type {BodylessMethod} */
-  api.get = (url, options) => api(url, ...options, 'get')
+  api.get = (url, options = {}) => api(url, options, 'get')
 
   /** @public @type {BodylessMethod} */
-  api.delete = (url, options) => api(url, ...options, 'delete')
+  api.delete = (url, options = {}) => api(url, options, 'delete')
 
   /** @public @type {BodylessMethod} */
-  api.head = (url, options) => api(url, ...options, 'head')
+  api.head = (url, options = {}) => api(url, options, 'head')
 
   /** @public @type {BodylessMethod} */
-  api.options = (url, options) => api(url, ...options, 'options')
+  api.options = (url, options = {}) => api(url, options, 'options')
 
   /** @public @type {BodyMethod} */
-  api.post = (url, data, options) => api(url, ...options, 'post', data)
+  api.post = (url, data, options = {}) => api(url, options, 'post', data)
 
   /** @public @type {BodyMethod} */
-  api.put = (url, data, options) => api(url, ...options, 'put', data)
+  api.put = (url, data, options = {}) => api(url, options, 'put', data)
 
   /** @public @type {BodyMethod} */
-  api.patch = (url, data, options) => api(url, ...options, 'patch', data)
+  api.patch = (url, data, options = {}) => api(url, options, 'patch', data)
 
   /** @public */
   api.all = Promise.all.bind(Promise)
